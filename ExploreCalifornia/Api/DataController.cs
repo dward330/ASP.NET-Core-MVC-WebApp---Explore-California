af0a4188c43533a336b4019cc7e113289f038c0e
Derrick Kyle Ward <dward330@gmail.com>
2018-06-12 16:21:48 -0400

    Added Testing Project and SQL Script
    Added Testing Project and SQL Script

---
+++ ExploreCalifornia/Api/DataController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ExploreCalifornia.DAL;
+using ExploreCalifornia.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExploreCalifornia.Api
+{
+    [Produces("application/json")]
+    [Route("api/Data")]
+    public class DataController : Controller
+    {
+        private readonly SpecialsDataContext _SpecialsDb;
+
+        public DataController(SpecialsDataContext specialsDb)
+        {
+            this._SpecialsDb = specialsDb;
+        }
+
+        [HttpGet]
+        [Route("GetMonthlySpecials")]
+        public JsonResult GetMonthlySpecials()
+        {
+            IEnumerable<Special> data = null;
+            if (null != this._SpecialsDb)
+            {
+                data = this._SpecialsDb.GetMonthlySpecials();
+            }
+
+            HttpContext.Response.Headers.Add("Access-Control-Allow-Origin", "*");
+
+            return Json(data);
+        }
+    }
+}
